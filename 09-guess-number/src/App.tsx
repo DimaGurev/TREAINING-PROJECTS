3ca58c6c59536ed470c775f578fb1a343ee3d631
DimaGurev <124017827+DimaGurev@users.noreply.github.com>
2024-06-09 18:40:17 +1000

    09-guess-number
    Сделана вторая версия игры угадай число 09-guess-number

--- 09-guess-number/src/App.tsx
+++ 09-guess-number/src/App.tsx
@@ -1,35 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Импорт стилей
+
+// Импорт React и его компонентов
+import { useState } from "react";
+
+// Импорт компонентов приложения
+
+// Импорт библиотек и сторонних зависимостей
+
+// Импорт типов и интерфейсов
+
+// Импорт утилит и вспомогательных функций
+
+// Импорт констант и перечислений
+
+// Импорт изображений, шрифтов и других ресурсов
+
+function getRandomNumber(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [userName, setUsetName] = useState("");
+  const [value, setValue] = useState("");
+  const [answerOptions, setAnswerOptions] = useState<number[]>([]);
+  const [randomNumber] = useState(getRandomNumber(0, 100));
+  const [gameIsClosed, setGameIsClosed] = useState(false);
+
+  const submitForm = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    if (!userName) {
+      setUsetName(value);
+    }
+    if (userName) {
+      setAnswerOptions((prev) => [...prev, +value]);
+    }
+    if (userName && +value === randomNumber) {
+      setGameIsClosed(true);
+    }
+    setValue("");
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <h1>🎲 Guess Number {randomNumber}</h1>
+      {userName && (
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          😄 {userName}, there is a number between 0 and 100. Try to guess it in
+          the fewest number of tries. After each attempt, there will be a
+          message with the text - 'Few', 'Many' or 'Right'.'
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      )}
+      {answerOptions.map((item, index) => (
+        <div key={index}>
+          <p>➡️ {item}</p>
+
+          {item !== randomNumber && (
+            <>
+              {item < randomNumber ? (
+                <p>⬆️️ Few. Try again 😸</p>
+              ) : (
+                <p>⬆️️ Many. Try again 😸</p>
+              )}
+            </>
+          )}
+        </div>
+      ))}
+
+      {gameIsClosed && (
+        <>
+          <hr />
+          <p>🎊 Right. The number you've guessed: {randomNumber}</p>
+          <p>🎉 Number of attempts: {answerOptions.length}</p>
+          <button onClick={() => window.location.reload()}>Play again?</button>
+        </>
+      )}
+      {!gameIsClosed && (
+        <form onSubmit={submitForm}>
+          <input
+            max={100}
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+            type={userName ? "number" : "text"}
+            placeholder={userName ? "Enter number" : "👋 Enter your name"}
+          />
+        </form>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
